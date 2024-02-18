ab5125189da9b5694f057e1bc4db5b14fa9cb4a7
qianqianzyk <3265569512@qq.com>
2024-02-18 23:35:51 +0800

    feat:新增教师查询学生信息接口

--- app/controllers/teacherController/studentList.go
+++ app/controllers/teacherController/studentList.go
@@ -5,6 +5,7 @@ import (
 	"SelectionSystem-Back/app/services/teacherService"
 	"SelectionSystem-Back/app/utils"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type Student struct {
@@ -189,4 +190,106 @@ func GetUltimateStudentList(c *gin.Context) {
 		"student_num": num,
 		"data":        responseStudentList,
 	})
+}
+
+type StudentData struct {
+	Name            string `json:"name"`
+	StudentID       string `json:"studentID"`
+	Class           string `json:"class"`
+	Phone           string `json:"phone"`
+	PoliticalStatus string `json:"political_status"`
+	Email           string `json:"email"`
+	Address         string `json:"address"`
+	Plan            string `json:"plan"`
+	Experience      string `json:"experience"`
+	Honor           string `json:"honor"`
+	Interest        string `json:"interest"`
+	Avatar          string `json:"avartar"`
+	TeacherName     string `json:"teacher_name"`
+	TargetName      string `json:"target_name"`
+	TargetAgree     int    `json:"target_agree"`
+	AdminAgree      int    `json:"admin_agree"`
+	SelectionTable  string `json:"selection_table"`
+}
+
+type StudentIDData struct {
+	StudentID string `form:"student_id" binding:"required"`
+}
+
+// 获取学生信息
+func GetStudentInfo(c *gin.Context) {
+	studentId := c.Query("student_id")
+	userId, er := c.Get("ID")
+	if !er {
+		utils.JsonErrorResponse(c, apiException.ServerError)
+		return
+	}
+
+	user, err := teacherService.GetUserByID(userId.(int))
+	if err != nil {
+		utils.JsonErrorResponse(c, apiException.ServerError)
+		return
+	}
+
+	if user.Type != 2 && user.Type != 3 {
+		utils.JsonErrorResponse(c, apiException.ServerError)
+		return
+	}
+
+	student, err := teacherService.GetStudentInfoByStudentID(studentId)
+	if err != nil {
+		utils.JsonErrorResponse(c, apiException.ServerError)
+		return
+	}
+
+	studentInfo, err := teacherService.GetUserByID(student.UserID)
+	if err != nil {
+		utils.JsonErrorResponse(c, apiException.ServerError)
+		return
+	}
+
+	var targetTeacherName string
+	if student.TargetID == 0 {
+		targetTeacherName = "无"
+	} else {
+		targetTeacher, _, err := teacherService.GetTeacherByID(student.TargetID)
+		if err != nil && err != gorm.ErrRecordNotFound {
+			utils.JsonErrorResponse(c, apiException.ServerError)
+			return
+		}
+		targetTeacherName = targetTeacher.TeacherName
+	}
+	var ultimateTeacherName string
+	if student.TeacherID == 0 {
+		ultimateTeacherName = "无"
+	} else {
+		ultimateTeacher, _, err := teacherService.GetTeacherByID(student.TeacherID)
+		if err != nil && err != gorm.ErrRecordNotFound {
+			utils.JsonErrorResponse(c, apiException.ServerError)
+			return
+		}
+		ultimateTeacherName = ultimateTeacher.TeacherName
+	}
+
+	response := StudentData{
+		StudentID:       student.StudentID,
+		Name:            student.Name,
+		Class:           student.Class,
+		Phone:           student.Phone,
+		PoliticalStatus: student.PoliticalStatus,
+		Email:           student.Email,
+		Address:         student.Address,
+		Plan:            student.Plan,
+		Experience:      student.Experience,
+		Honor:           student.Honor,
+		Interest:        student.Interest,
+		Avatar:          studentInfo.Avartar,
+		TeacherName:     ultimateTeacherName,
+		TargetName:      targetTeacherName,
+		TargetAgree:     student.TargetStatus,
+		AdminAgree:      student.AdminStatus,
+		SelectionTable:  student.SelectionTable,
+	}
+
+	utils.JsonSuccessResponse(c, response)
 }
