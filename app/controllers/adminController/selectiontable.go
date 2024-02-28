8ea4f396f65eab39d18ea97bc4def6e8e84b2e20
Penryn <1515805213@163.com>
2024-02-28 21:54:52 +0800

    fix:管理员拒绝增加理由编号

--- app/controllers/adminController/selectiontable.go
+++ app/controllers/adminController/selectiontable.go
@@ -56,6 +56,7 @@ func GetTable(c *gin.Context) {
 
 type CheckTableData struct {
 	StudentsID []string `json:"students_id" validate:"required"`
+	ReasonID  int      `json:"reason_id" validate:"required"`
 	Check      int      `json:"check" validate:"oneof=1 2"` // 1:同意 2:拒绝
 }
 
@@ -109,6 +110,18 @@ func CheckTable(c *gin.Context) {
 		if err != nil {
 			utils.JsonErrorResponse(c, apiException.ServerError)
 			return
+		}
+		if data.Check==2{
+			reason ,err:= userService.GetReasonsByReasonID(data.ReasonID)
+			if err != nil {
+				utils.JsonErrorResponse(c, apiException.ServerError)
+				return
+			}
+			err = userService.SendConversation(user.ID, student.UserID, "您的双向申请表申请被拒绝，原因："+reason.ReasonContent)
+			if err != nil {
+				utils.JsonErrorResponse(c, apiException.ServerError)
+				return
+			}
 		}
 	}
 	utils.JsonSuccessResponse(c, nil)
